7227eb31c3562288d035be655714d47c23de130a
PhanTuan <tuanpvph37169@fpt.edu.vn>
2024-10-09 13:52:22 +0700

    Commit new

--- src/main/java/com/example/demo/holo/phantun.java
+++ src/main/java/com/example/demo/holo/phantun.java
@@ -2,8 +2,6 @@ package com.example.demo.holo;
 
 public class phantun {
     String a ="ádlkfjsldkf";
-<<<<<<< HEAD
     String c ="commit thanh cong";
-=======
->>>>>>> c27bbae3415991eca26a4adabe0b58be419bccf9
+    String v = "alksdlfjdf";
 }
